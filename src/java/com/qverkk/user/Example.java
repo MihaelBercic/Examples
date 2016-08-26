ac87365d22cf8316f79f0fff2382b78d2b5214a0
Mihael <mihael.valentin@gmail.com>
2016-08-26 17:55:37 +0200

    Example how to use classes.

---
+++ src/java/com/qverkk/user/Example.java
@@ -0,0 +1,31 @@
+package java.com.qverkk.user;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @Author Mihael
+ * 
+ */
+
+public class Example {
+
+	public Example() {
+		String[] file = new String[] { "username,password,393,1,mulename", "username,password,393,1,mulename", "username,password,393,1,mulename", "username,password,393,1,mulename" };
+		List<User> users = new ArrayList<>();
+		for (String s : file) {
+			String[] params = s.split(",");
+			String username = params[0];
+			String password = params[1];
+			int world = Integer.parseInt(params[2]);
+			int oreNumber = Integer.parseInt(params[3]);
+			String muleName = params[4];
+			users.add(new User(username, password, world, oreNumber, muleName));
+		}
+
+		for (User user : users) {
+			System.out.println("Launch client: " + user.getUsername());
+		}
+
+	}
+}
---
+++ src/java/com/qverkk/user/Main.java
@@ -0,0 +1,16 @@
+package java.com.qverkk.user;
+
+/*
+ * @Author Mihael
+ * 
+ */
+
+public class Main {
+	
+	// you can ignore this file as it is not important in your case @qverkk.
+
+	public static void main(String[] args) {
+		Example test = new Example();
+	}
+
+}
