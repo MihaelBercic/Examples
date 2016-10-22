e2da6ad0898dd5a3da1ca26deb7eefd9de9a539a
Mihael <mihael.valentin@gmail.com>
2016-10-22 22:13:15 +0200

    Add

--- src/code/mihael/query/Main.java
+++ src/code/mihael/query/Main.java
@@ -7,7 +7,6 @@ public class Main {
 	public static void main(String... args) {
 		long start = System.currentTimeMillis();
 		
-		
 		Person p = Person.newQuery().names("Mihael").results().first();
 		System.out.println(p.getName() + " -> " + p.getAge());
 
